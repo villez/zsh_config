fc232b44a9fb3b7fa2699db6a0a1a76a2bebe847
Ville Siltanen <ville.siltanen@iki.fi>
2013-10-05 21:23:33 +0300

    platform-specific lscolors

--- exports.zsh
+++ exports.zsh
@@ -3,7 +3,11 @@ export LANG='en_US.UTF-8'
 export LC_CTYPE='fi_FI.UTF-8'
 
 export CLICOLOR=1
-export LSCOLORS="exfxcxdxcxegedabagexex"
+if [[ $PLATFORM_OSX -eq 1 ]]; then
+    export LSCOLORS="exfxcxdxcxegedabagexex"
+elif [[ $PLATFORM_LINUX -eq 1 ]]; then
+    export LS_COLORS='rs=0:di=00;34:ln=00;35:mh=00:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;42:ow=00;34:st=37;44:ex=00;32:'
+fi
 
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'

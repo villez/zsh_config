3b8509e3d149830c8abe77792b8c7cd9006895e0
Ville Siltanen <ville.siltanen@iki.fi>
2013-10-05 19:13:48 +0300

    further exploration of ls aliases and functions

--- aliases.zsh
+++ aliases.zsh
@@ -2,9 +2,6 @@
 alias ..='cd ..'
 alias ...='cd ../../'
 alias history='fc -l 1'
-alias l='ls -laGFh'
-alias lld='ls -laGFhd */ | less'
-alias llh='ls -laGFhd .* | less'
 alias md='mkdir -pv'
 alias q='exit'
 alias rake='noglob rake'  # allow task[param] argument format
@@ -16,24 +13,37 @@ alias sr='screen -D -R'
 alias topc='top -o cpu'
 alias treed='tree -d --charset=utf-16 -C'
 
-# if no params or param a directory -> pipe to less
-# use the special environment variable CLICOLOR_FORCE to
-# make ls use colors when the output is non-terminal
+
+# handling the different color options between BSD/OSX vs GNU/Linux ls
+# an alternative to this mess would be to use the GNU coreutils ls
+# also on OS X
+if [ $PLATFORM_OSX -eq 1 ]; then
+    alias l='CLICOLOR_FORCE=1 ls -laGFh'
+elif [ $PLATFORM_LINUX -eq 1 ]; then
+    alias l='ls -laFh --color=always'
+fi
+alias lld='l -d */ | less'
+alias llh='l -d .* | less'
+
+# if no params given or param a directory -> pipe output less;
+# if a non-directory param given, no paging,
 ll() {
     # if there are parameters
     if [ $# -gt 0 ]; then
 	# if a directory given
 	if [ -d $1 ]; then
-	    CLICOLOR_FORCE=1 l $* | less
+	    l $* | less
 	# some params were given, but not a directory
 	else
 	    l $*
 	fi
     # no params given
     else
-	CLICOLOR_FORCE=1 l | less
+	l | less
     fi
 }
+
+
 
 # create new directory and cd into it
 take() {
@@ -56,7 +66,7 @@ llc() {
 # list only directories from given dir
 lldd() {
   cd $1
-  ls -laGFhd */
+  lld
   cd -
 }
 

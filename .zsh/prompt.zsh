4bc3c72abac136189c7cc671e99b4fe4eddc7380
Ville Siltanen <ville.siltanen@iki.fi>
2013-10-08 23:20:18 +0300

    ahead/behind remote indicator to prompt git status

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -7,6 +7,8 @@ ZSH_THEME_GIT_PROMPT_MODIFIED="*"
 ZSH_THEME_GIT_PROMPT_RENAMED="~"
 ZSH_THEME_GIT_PROMPT_DELETED="!"
 ZSH_THEME_GIT_PROMPT_UNMERGED="?"
+ZSH_THEME_GIT_PROMPT_AHEAD=">"
+ZSH_THEME_GIT_PROMPT_BEHIND="<"
 
 my_git_prompt_info() {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || return
@@ -44,8 +46,23 @@ git_prompt_status() {
   if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
   fi
+
+  local ahead behind remote
+  local -a gitstatus
+
+  remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} \
+      --symbolic-full-name 2>/dev/null)/refs\/remotes\/}
+
+  if [[ -n ${remote} ]] ; then
+      ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
+      (( $ahead )) && STATUS="$ZSH_THEME_GIT_PROMPT_AHEAD$STATUS"
+
+      behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
+      (( $behind )) && STATUS="$ZSH_THEME_GIT_PROMPT_BEHIND$STATUS"
+  fi
   echo $STATUS
 }
+
 
 
 PROMPT='[$PR_BOLD_GREEN%n@%m:%{$reset_color%}'           # user@host (green)

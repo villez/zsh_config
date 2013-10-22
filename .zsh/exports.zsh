54becdcf1e2c382bb563c9e499fd0f2c3d2d4cf5
Ville Siltanen <ville.siltanen@iki.fi>
2013-10-22 10:00:14 +0300

    make completion use LS_COLORS

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -3,14 +3,17 @@ export LANG='en_US.UTF-8'
 export LC_CTYPE='fi_FI.UTF-8'
 
 # configuring colors for ls; different format for BSD/OSX and GNU/Linux versions
-# the two versions are currently not 1:1 in all details but match for the most
-# important cases - directories, executables, symlinks and the like
-export CLICOLOR=1
+# the two configs here are currently not 1:1 in all details but match well enough
+# for the most important cases - normal files, directories, executables, symlinks
 if [[ $PLATFORM_OSX -eq 1 ]]; then
+    export CLICOLOR=1
     export LSCOLORS="exfxcxdxcxegedabagexex"
-elif [[ $PLATFORM_LINUX -eq 1 ]]; then
-    export LS_COLORS='rs=0:di=00;34:ln=00;35:mh=00:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;42:ow=00;34:st=37;44:ex=00;32:'
 fi
+
+# even though BSD ls uses the LSCOLORS version above, export this one
+# on OS X as well so we can reuse this for zsh completion and get the same
+# coloring for file/directory name completion
+export LS_COLORS='rs=0:di=00;34:ln=00;35:mh=00:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;42:ow=00;34:st=37;44:ex=00;32:'
 
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'
@@ -18,14 +21,14 @@ export GREP_COLOR='1;32'
 export LESS="-eXFR"
 
 export PAGER='less'
-export EDITOR='~/bin/emacsclient'
+export EDITOR='~/bin/ec'
 
 export LC_COLLATE=C
 
 
 # setting up the path
-# common: add ~/bin
 
+# add ~/bin
 export PATH="$PATH:$HOME/bin"
 
 # rbenv path + initialization

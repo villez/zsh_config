e61c5209745cd18d0d76ce6fa1c0e8ea03319d51
Ville Siltanen <ville.siltanen@iki.fi>
2013-10-28 22:07:22 +0200

    fine-tuning options

--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -1,16 +1,25 @@
+# quiet!
 setopt no_beep
 
+# filename generation
 setopt extended_glob
 
+# history
 setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
+setopt hist_no_store
+setopt hist_verify
 
+# completion
 setopt always_to_end
 setopt auto_menu
 setopt complete_in_word
 unsetopt menu_complete
 
+# spelling correction for commands
 setopt correct
 
+# prompt: enable parameter expansion, command substitution, and
+# arithmetic expansion
 setopt prompt_subst
